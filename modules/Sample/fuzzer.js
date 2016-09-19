dbde08e3bd2e7358ddcea296f1110a94acc47922
Christoph Diehl <cdiehl@mozilla.com>
2016-09-20 01:20:53 +0200

    Add template for modules

---
+++ modules/Sample/fuzzer.js
@@ -0,0 +1,64 @@
+/*
+ * XyzAPI References
+ *
+ * WebIDL:
+ *
+**/
+
+var fuzzerXyz = (function() {
+  /*
+  ** Initialization
+  ** Commands which shall be called at the beginning of a testcase.
+  */
+  function onInit()
+  {
+    var cmd = [];
+
+    return cmd;
+  }
+
+  /*
+  ** Main
+  ** Command which shall be called after initialization.
+  */
+  function makeCommand()
+  {
+    var cmd = [];
+
+    return cmd;
+  }
+
+  /*
+  ** De-initialization.
+  ** Commands which shall be called at the end of a testcase.
+  */
+  function onFinish()
+  {
+    var cmd = [];
+
+    return cmd;
+  }
+
+  /*
+  ** Methods and attributes.
+  */
+  var AnimatableMethods = {
+    "name": []
+  };
+
+  var AnimationAttributes = {
+    "name": function() { },
+  };
+
+  var Events = {
+    "object_name": ['name'],
+  };
+
+  return {
+    onInit: onInit,
+    makeCommand: makeCommand,
+    onFinish: onFinish,
+    Events: Events
+  };
+})();
+
