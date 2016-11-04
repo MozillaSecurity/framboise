700ff10e5bb915224c31b85d259cf656144fddc9
Christoph Diehl <cdiehl@mozilla.com>
2016-11-04 04:08:55 +0100

    Use shared configuration files provided in FuzzData

--- init.sh
+++
@@ -1,2 +0,0 @@
-curl https://raw.githubusercontent.com/MozillaSecurity/fuzzdata/master/settings/firefox/prefs.js --create-dirs -o settings/firefox/prefs.js
-curl https://raw.githubusercontent.com/MozillaSecurity/fuzzdata/master/settings/iexplorer/enable-active-content.reg --create-dirs -o settings/iexplorer/enable-active-content.reg
